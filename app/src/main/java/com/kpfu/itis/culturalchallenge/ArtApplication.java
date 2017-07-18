105e2f1c815f9e8a01a6144349223aa4ffcc5693
YuraKhan <m_200_6@mail.ru>
2017-07-18 19:08:03 +0300

    fix gradle

---
+++ app/src/main/java/com/kpfu/itis/culturalchallenge/ArtApplication.java
@@ -0,0 +1,16 @@
+package com.kpfu.itis.culturalchallenge;
+
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by Rage on 18.07.2017.
+ */
+
+public class ArtApplication extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
