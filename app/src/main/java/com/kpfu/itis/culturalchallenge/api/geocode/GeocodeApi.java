a2bfeff275ba256a9a30ad063ffe9a696e64319d
YuraKhan <m_200_6@mail.ru>
2017-07-19 01:23:19 +0300

    fix geocode

--- app/src/main/java/com/kpfu/itis/culturalchallenge/api/geocode/GeocodeApi.java
+++ app/src/main/java/com/kpfu/itis/culturalchallenge/api/geocode/GeocodeApi.java
@@ -1,5 +1,7 @@
 package com.kpfu.itis.culturalchallenge.api.geocode;
 
+import android.util.Log;
+
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import com.kpfu.itis.culturalchallenge.api.geocode.pojo.GeocodeResponse;
 import com.kpfu.itis.culturalchallenge.api.geocode.pojo.Location;
@@ -15,8 +17,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class GeocodeApi {
 
-    private static final String API_KEY = "AIzaSyCxLZq0WA8jk3fuBlOFfrqVe79ZqHOGUXQ";
-    private static final String BASE_URL = "http://maps.googleapis.com/maps/api/geocode/";
+    private static final String API_KEY = "AIzaSyBeKQHjAxJcITvnJK0Tg5Pr9iLkEJnp_4I";
+    private static final String BASE_URL = "https://maps.googleapis.com/maps/api/geocode/";
     private GeocodeRequest mGeocodeRequest;
 
     public GeocodeApi() {
@@ -34,6 +36,7 @@ public class GeocodeApi {
                 =mGeocodeRequest.getGeocode(request,API_KEY);
         return geocodeResponseObservable.flatMap(
                 geocodeResponse -> Observable.<Location>create(e -> {
+                    Log.v("STTT",geocodeResponse.getStatus());
                     if(geocodeResponse.getStatus().equals("OK")){
                         if(geocodeResponse.getResults().isEmpty()){
                             Location location=new Location();
@@ -41,6 +44,7 @@ public class GeocodeApi {
                             e.onNext(location);
                         }else {
                             for (Result result : geocodeResponse.getResults()) {
+                                Log.v("STTT",result.getFormattedAddress());
                                 e.onNext(result.getGeometry().getLocation());
                             }
                         }
