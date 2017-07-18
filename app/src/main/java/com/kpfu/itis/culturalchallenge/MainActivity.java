7fbd7fc21bef8d9ba086f17ffabc79d93bd6b308
YuraKhan <m_200_6@mail.ru>
2017-07-18 22:10:02 +0300

    request location permissions

--- app/src/main/java/com/kpfu/itis/culturalchallenge/MainActivity.java
+++ app/src/main/java/com/kpfu/itis/culturalchallenge/MainActivity.java
@@ -1,6 +1,10 @@
 package com.kpfu.itis.culturalchallenge;
 
+import android.*;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -67,7 +71,9 @@ public class MainActivity extends AppCompatActivity implements TabViewWrapper.Ta
 
             }
         });
-
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION},0);
+        }
     }
 
     @Override
